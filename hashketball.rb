b1291a75fd0868fabe97a654446d6216f4cbbbd0
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-07 20:18:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,8 @@ def game_hash
   game_hash = { :home => { 
     :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
-    :players => [{:player_name => "Alan Anderson", 
+    :players => [{:player_name => "Alan Anderson", },
+    {:player_name => "Reggie Evans", }, {:player_name => "Brook Lopez", }, 
     
       ] 
     
