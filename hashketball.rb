597e3006ca70682894aa3694af3b006cd62653a3
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-07 19:53:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,29 @@
 # Write your code here!
+require 'pry'
+
+def game_hash
+  
+  game_hash = { :home => { 
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black", "White"],
+    :players => [] 
+    
+  },
+  :away => { 
+    :team_name => "Charlotte Hornets", 
+    :colors => ["Turquoise", "Purple"],
+    :players => []
+    
+  }
+    
+    
+  }
+  
+  
+  
+  
+  
+  
+ return game_hash 
+  
+end
